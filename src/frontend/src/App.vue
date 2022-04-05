fea00b174473781bc470aa5b0f89aa9b4494b9e0
topsv <topsv@hismith.ru>
2022-04-05 17:23:02 +0300

    Добавляет данные из json

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -18,12 +18,15 @@
         </p>
       </div>
     </div>
+    <Index />
   </div>
 </template>
 
 <script>
+import Index from "@/views/Index.vue";
 export default {
   name: "App",
+  components: { Index },
 };
 </script>
 
---
+++ src/frontend/src/views/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="misc in miscs" :key="misc.id">
+      {{ misc }}
+    </div>
+    <div v-for="pizza in pizzas" :key="pizza.id">
+      {{ pizza }}
+    </div>
+    <div v-for="user in users" :key="user.id">
+      {{ user }}
+    </div>
+  </div>
+</template>
+<script>
+import misc from "@/static/misc.json";
+import pizza from "@/static/pizza.json";
+import user from "@/static/user.json";
+
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      miscs: misc,
+      pizzas: pizza,
+      users: user,
+    };
+  },
+};
+</script>
